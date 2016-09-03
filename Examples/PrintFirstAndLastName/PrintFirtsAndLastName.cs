3d1e2ffdb7b2d3aa09586d1c9876c01acbf94718
bozhinov86 <mitko_bozhinov@abv.bg>
2016-09-03 16:01:52 +0300

    PrintFirstAndLastName

---
+++ Examples/PrintFirstAndLastName/PrintFirtsAndLastName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+    class PrintFirtstAndLastName
+    {
+        static void Main()
+        {
+            string firstName = "Mitko";
+            string lastName = "Bozhinov";
+            Console.WriteLine("My First name is: "+ firstName);
+            Console.WriteLine("My Last name is: " + lastName);
+        }
+    }
