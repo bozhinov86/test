53c02a1a0833e14dd1749dabb64379e03362461f
bozhinov86 <mitko_bozhinov@abv.bg>
2016-09-03 15:02:56 +0300

    Print my name example

---
+++ Examples/PrintMyName/PrintMyName.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+ class PrintMyName
+    {
+        static void Main()
+        {
+            string myName ="My name is Mitko Bozhinov";
+            Console.WriteLine(myName);
+        }
+    }
