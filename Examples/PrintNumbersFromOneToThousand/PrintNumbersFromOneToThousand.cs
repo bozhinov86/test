07f2129c666af45bc1bdf33f17897c86b7b91c35
bozhinov86 <mitko_bozhinov@abv.bg>
2016-09-06 17:00:59 +0300

    PrintNumbersFromOneToThousand

---
+++ Examples/PrintNumbersFromOneToThousand/PrintNumbersFromOneToThousand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+  class PrintNumbersFromOneToThousand
+    {
+        static void Main()
+        {
+            Console.BufferHeight = 1001;
+            for (int i = 1; i <= 1000; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
