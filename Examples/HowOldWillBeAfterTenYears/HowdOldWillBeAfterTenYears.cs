7abbf3460ccf52778f158d2d7b0e3f62cfcaf92d
bozhinov86 <mitko_bozhinov@abv.bg>
2016-09-06 15:58:37 +0300

    HowOldWillBeAfterTenYears

---
+++ Examples/HowOldWillBeAfterTenYears/HowdOldWillBeAfterTenYears.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+    class HowdOldWillBeAfterTenYears
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Въведете дата си на раждане:");
+            DateTime inputYear = DateTime.Parse(Console.ReadLine());
+            DateTime now = DateTime.Now;
+            int myAge = now.Year - inputYear.Year;
+            int afterTenYears = myAge + 10;
+            Console.WriteLine("Аз съм на {0} годинишна възраст и след 10 години ще бъда на {1} години !!! ", myAge, afterTenYears);
+        }
+    }
