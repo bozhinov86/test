0e2f74f0481afa8116f110fbdb700be542124e63
bozhinov86 <mitko_bozhinov@abv.bg>
2016-09-06 16:48:09 +0300

    Long Sequence

---
+++ Examples/PrintLongSquence/LongSquence.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LongSequence
+    {
+        static void Main()
+        {
+            List<int> listNumbers = new List<int>();
+            for (int i = 2; i < 1000; i++)
+            {
+                
+                if (i % 2 == 0)
+                {
+                    listNumbers.Add(i);
+                }
+                else
+                {
+                    listNumbers.Add(-i);
+                }
+            }
+            foreach (int item in listNumbers)
+            {
+                Console.Write(item + ",");
+            } 
+        }
+    }
