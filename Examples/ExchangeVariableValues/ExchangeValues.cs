d2b9827e9702ad650eb09d63600a2061cdf9f253
bozhinov86 <mitko_bozhinov@abv.bg>
2016-09-07 21:33:48 +0300

    Exchange Values

---
+++ Examples/ExchangeVariableValues/ExchangeValues.cs
@@ -0,0 +1,14 @@
+﻿using System;
+  class ExchangeValues
+    {
+        static void Main()
+        {
+            int a = 5;
+            int b = 10;
+            Console.WriteLine("Values before exchange with values {0} and {1}", a,b);
+            b -= a;
+            a += a;
+            Console.WriteLine("Values after exchange with values {0} and {1}", a, b);
+        }
+    }
+
