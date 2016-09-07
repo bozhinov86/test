498ba2ab224bb4eb187041639ce6daeef69f2397
bozhinov86 <mitko_bozhinov@abv.bg>
2016-09-07 16:02:08 +0300

    Hello Gender

---
+++ Examples/BoolDataType/HelloGender.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+class HelloGender
+    {
+        static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("Вашият пол е мъжки(M) или женски(F):");
+        string gender = Console.ReadLine();
+        bool isFemale = (gender == "F" || gender == "f");
+        bool isMale = (gender == "M" || gender == "m");
+        if (isFemale)
+        {
+            Console.WriteLine("Добър ден г-жа..");
+        }
+        else if(isMale)
+        {
+            Console.WriteLine("Добър ден г-н...");
+        }
+        else
+        {
+            Console.WriteLine("Моля, Въведете един от символите(M/F)"); 
+        }
+    }
+    }
+
