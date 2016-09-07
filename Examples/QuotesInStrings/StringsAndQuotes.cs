f468e5411ae0332cf67e1fdbe8b43af01c1da52e
bozhinov86 <mitko_bozhinov@abv.bg>
2016-09-07 21:00:46 +0300

    StringsAndQuotes

---
+++ Examples/QuotesInStrings/StringsAndQuotes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+    class StringsAndQuotes
+    {
+        static void Main()
+        {
+            string strOne = "The \"use\" of quotations causes difficulties";
+            string strTwo = @"The ""use"" of quotations causes difficulties";
+            Console.WriteLine(strOne);
+            Console.WriteLine(strTwo);
+        }
+    }
