2f4cb2b8ef33b6e3d7e518cac242b8def8d2f8d0
bozhinov86 <mitko_bozhinov@abv.bg>
2016-09-07 20:57:03 +0300

    IsoscelesTriangle

---
+++ Examples/Isosceles Triangle/IsoscelesTriangle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+
+    class IsoscelesTriangle
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            char symbol = '\u00a9';
+            Console.WriteLine("{0,10}\n\n{0,8}{0,4}\n\n{0,6}{0,8}\n\n{0,4}{0,4}{0,4}{0,4}", symbol);
+        }
+    }
