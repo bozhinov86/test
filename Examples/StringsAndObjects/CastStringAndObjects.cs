6e5fab615f1d5b071a2da2535666c1e34f18fabf
bozhinov86 <mitko_bozhinov@abv.bg>
2016-09-07 16:10:24 +0300

    String and Objects cast

---
+++ Examples/StringsAndObjects/CastStringAndObjects.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+    class CastStringAndObjects
+    {
+        static void Main()
+        {
+            string hello = "Hello";
+            string world = "World";
+            object helloWorld = hello + " " + world;
+            string result = helloWorld.ToString();
+            Console.WriteLine(result);
+        }
+    }
